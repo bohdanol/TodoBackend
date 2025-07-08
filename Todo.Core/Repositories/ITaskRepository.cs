99207da58130f5ee9b532f10d4cf554e2240bf98
rarik <rarik@DESKTOP-KM2P0BL>
2025-07-08 15:19:33 +0300

    add TaskRepository

---
+++ Todo.Core/Repositories/ITaskRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Core.Repositories;
+
+public interface  ITaskRepository
+{
+    Task<Models.Task> GetByIdAsync(int id);
+    Task<IEnumerable<Models.Task>> GetAllAsync();
+    Task AddAsync(Models.Task task);
+    Task UpdateAsync(Models.Task task);
+    Task DeleteAsync(int id);
+}
---
+++ Todo.Core/Repositories/TaskRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Core.Data;
+
+namespace Todo.Core.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+
+        private readonly TodoDbContext _context;
+
+        public TaskRepository(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Models.Task task)
+        {
+            await _context.Task.AddAsync(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var task = await _context.Task.FindAsync(id);
+            if (task != null) 
+            { 
+                _context.Task.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Models.Task>> GetAllAsync()
+        {
+            return await _context.Task.ToListAsync();
+        }
+
+        public async Task<Models.Task> GetByIdAsync(int id)
+        {
+            return await _context.Task.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Models.Task task)
+        {
+            var existingTask = await _context.Task.FindAsync(task.Id);
+            if (existingTask == null)
+            {
+                throw new Exception($"Task with ID {task.Id} not found.");
+            }
+
+            existingTask.Title = task.Title;
+            existingTask.Description = task.Description;
+            existingTask.IsCompleted = task.IsCompleted;
+            existingTask.Priority = task.Priority;
+            existingTask.UpdatedAt = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
